c8647834bcabb5d5c3d116c1b4ec4a60673741b1
mammix2 <mammix2@hotmail.com>
2017-06-01 16:51:38 +0100

    Change ID to Core

--- src/version.cpp
+++ src/version.cpp
@@ -36,7 +36,7 @@ const std::string CLIENT_NAME("Fibre");
 // git will put "#define GIT_ARCHIVE 1" on the next line inside archives. 
 #define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE
-#    define GIT_COMMIT_ID ": Final"
+#    define GIT_COMMIT_ID ": Core"
 //#    define GIT_COMMIT_DATE "$Format:%cD"
 #endif
 
